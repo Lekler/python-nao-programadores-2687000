626fc2841fc11d0bd8ea719cbe2f2d345a44ce1b
Alexandre Lekler Rodrigues <alexandrebicordeiro@hotmail.com>
2024-06-26 21:36:49 +0000

    Resoluçãqo do Exercicio

--- exercicios/1-primeiros-passos/1-variaveis.py
+++ exercicios/1-primeiros-passos/1-variaveis.py
@@ -4,8 +4,23 @@
 # - E não podemos usar palavras reservadas do Python, como if, for, from, etc. 
 
 # Declare variáveis do tipo string
+nome = 'Lekler'
 
+profissao = "Estudante"
+
+objetivo = '''
+Aprender python
+para Data Science
+and AI.
+'''
 # Declare variáveis de cada tipo numérico
+idade = 13
 
+salario = 123.00
+
+complexo = 3+1j
 
 # Declare uma variável do tipo booleano
+verdadeiro = True
+
+falso = False
